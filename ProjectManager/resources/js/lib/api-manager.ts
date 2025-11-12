01147305ca361e7cbb5f3d52eee68ce5a363f8ed
Kasper Roager <kasper@ulvedalen.dk>
2025-11-12 08:50:20 +0100

    Updated task requests. create project requests.

--- ProjectManager/resources/js/lib/api-manager.ts
+++ ProjectManager/resources/js/lib/api-manager.ts
@@ -1,10 +1,10 @@
-import {ITask} from "@/types/types";
+import {IProject, ITask} from "@/types/types";
 import axios from "axios";
 
-const apiManager = {
+export const apiManager = {
     "task": {
         "create": (task: ITask) => {
-            return axios.post('/api/task', task);
+            return axios.post('/api/task/create', task);
         },
         "get": (taskId: number) => {
             return axios.get('/api/task/' + taskId);
@@ -13,10 +13,33 @@ const apiManager = {
             return axios.get('/api/task');
         },
         "update": (taskId: number, task: ITask) => {
-            return axios.put('/api/task/' + taskId, task);
+            return axios.patch('/api/task/edit/' + taskId, task);
+        },
+        "updateStatus": (taskId: number, status: string) => {
+            return axios.patch('/api/task/update_status/' + taskId, {"status": status});
         },
         "delete": (taskId: number) => {
             return axios.delete('/api/task/' + taskId);
+        }
+    },
+    "project": {
+        "create": (project: IProject) => {
+            return axios.post('/api/project/create', project);
+        },
+        "get": (projectId: number) => {
+            return axios.get('/api/project/' + projectId);
+        },
+        "getAll": () => {
+            return axios.get('/api/project');
+        },
+        "update": (projectId: number, project: IProject) => {
+            return axios.put('/api/project/' + projectId, project);
+        },
+        "updateStatus": (projectId: number, status: string) => {
+            return axios.patch('/api/project/update_status/' + projectId, {"status": status});
+        },
+        "delete": (projectId: number) => {
+            return axios.delete('/api/project/' + projectId);
         }
     }
 }
