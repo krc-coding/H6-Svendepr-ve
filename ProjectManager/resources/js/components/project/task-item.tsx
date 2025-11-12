bca831a842d9160445a494e161088376fcc7783d
Kasper Roager <kasper@ulvedalen.dk>
2025-11-12 08:53:48 +0100

    Removed comment

--- ProjectManager/resources/js/components/project/task-item.tsx
+++ ProjectManager/resources/js/components/project/task-item.tsx
@@ -4,17 +4,3 @@ const TaskItem = () => {
 
     return (<div>Base Project</div>);
 }
-
-/* Task example
-"data": {
-    "id": 1,
-    "title": "Test",
-    "description": "This is just a test to see if this works, i don't know if i do anytime wrong, or forgor something... sad",
-    "status": "Open",
-    "project_id": null,
-    "created_by": 1,
-    "due_date": "2026-04-22 14:00:00",
-    "created_at": "2025-11-11T09:59:33.000000Z",
-    "updated_at": "2025-11-11T09:59:33.000000Z"
-  }
- */
